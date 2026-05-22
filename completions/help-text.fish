f6690c02925422ab9f2d07874db01fe0835b3b61
Drazape <Drazape@outlook.com>
2026-05-22 10:46:58 +0530

    completions: common: alias → var

--- completions/help-text.fish
+++ completions/help-text.fish
@@ -1,6 +1,6 @@
-alias common-complete 'complete --command=help-text --no-files'
+set --local -- common_complete complete --command=help-text --no-files
 
-common-complete
-common-complete --long=help --short=h --description='Show this reference manual'
-common-complete --long=positional --short=p --description='Individual positional args' --exclusive
-common-complete --long=switch --short=s --description='Individual switches' --exclusive
+$common_complete
+$common_complete --long=help --short=h --description='Show this reference manual'
+$common_complete --long=positional --short=p --description='Individual positional args' --exclusive
+$common_complete --long=switch --short=s --description='Individual switches' --exclusive
