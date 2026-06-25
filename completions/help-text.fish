56224c7795376c3aa05d38b655351c6c66d49680
Drazape <Drazape@outlook.com>
2026-06-25 20:20:17 +0530

    completions: add sub-command

--- completions/help-text.fish
+++ completions/help-text.fish
@@ -2,5 +2,6 @@ set --local -- common_complete complete --command=help-text --no-files
 
 $common_complete
 $common_complete --long=help --short=h --description='Show this reference manual'
+$common_complete --long=sub-command --short=c --description='Individual sub-commands' --exclusive
 $common_complete --long=positional --short=p --description='Individual positional args' --exclusive
 $common_complete --long=switch --short=s --description='Individual switches' --exclusive
