b8ca3e89a8ef1adcd73675e18945459eee2507b1
Drazape <Drazape@outlook.com>
2026-07-01 10:10:05 +0530

    fix(calls): output-format: update subfunc name

--- functions/help-text/internal/output-format.fish
+++ functions/help-text/internal/output-format.fish
@@ -1,3 +1,3 @@
 function _help-text_internal_output-format --description='Slightly dim punctuation'
-    string trim -- {$argv} | string replace -- \; (_help-text_output-format_slight-dim \;) | string replace -- , (_help-text_output-format_slight-dim ,) | string replace -- . (_help-text_output-format_slight-dim .)
+    string trim -- {$argv} | string replace -- \; (_help-text_internal_output-format_slight-dim \;) | string replace -- , (_help-text_internal_output-format_slight-dim ,) | string replace -- . (_help-text_internal_output-format_slight-dim .)
 end
