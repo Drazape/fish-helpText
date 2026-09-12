f592dc490db45341d6407022e5c82b57ec98d269
Drazape <Drazape@outlook.com>
2026-09-12 20:16:58 +0530

    refactor(lib): internal: use format

--- functions/help-text/internal/italicize-names.fish
+++ functions/help-text/internal/italicize-names.fish
@@ -1,7 +1,7 @@
-    set --function italicized {$argv[-1]}
 function _help-text_internal_italicize-names --description='First take the Initial Arguments, then use them as the sub-strings to italicize in the full string (last argument)'
+    set --function -- italicized {$argv[-1]}
     for name in {$argv[1..-2]}
-        set --function -- italicized (string replace -- {$name} (set_color --italics){$name}(set_color --reset) {$italicized})
+        set --function -- italicized (string replace -- {$name} (format text italics {$name}) {$italicized})
     end
     echo {$italicized}
 end
