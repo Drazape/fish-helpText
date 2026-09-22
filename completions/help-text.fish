cc0019e321f73937d2e02ef3be22066648484497
Drazape <Drazape@outlook.com>
2026-09-22 19:54:16 +0530

    fix(completions): confine scope
    The completions file is directly scoped into the environment, having all its top-level local variables being loaded into the global environment.
    This is mitigated by wrapping the entire completions with `begin` and `end` blocks.

--- completions/help-text.fish
+++ completions/help-text.fish
@@ -1,7 +1,9 @@
-set --local -- common_complete complete --command=help-text --no-files
+begin
+    set --local -- common_complete complete --command=help-text --no-files
 
-$common_complete
-$common_complete --long=help --short=h --description='Show this reference manual'
-$common_complete --long=sub-command --short=c --description='Individual sub-commands' --exclusive
-$common_complete --long=positional --short=p --description='Individual positional args' --exclusive
-$common_complete --long=switch --short=s --description='Individual switches' --exclusive
+    $common_complete
+    $common_complete --long=help --short=h --description='Show this reference manual'
+    $common_complete --long=sub-command --short=c --description='Individual sub-commands' --exclusive
+    $common_complete --long=positional --short=p --description='Individual positional args' --exclusive
+    $common_complete --long=switch --short=s --description='Individual switches' --exclusive
+end
